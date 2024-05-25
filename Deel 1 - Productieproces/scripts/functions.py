39955b1e10ab6a31ea05d8bb9ef4e8492493f25a
Sarah De Ceuster <sarah.de.ceuster@gmail.com>
2024-05-25 18:47:34 +1000

    stilaan klaar.. alleen nog de yml file

--- Deel 1 - Productieproces/scripts/functions.py
+++ Deel 1 - Productieproces/scripts/functions.py
@@ -1,10 +1,19 @@
 import glob
 import json
+import os
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
 
 def read_daily(city):
     """
     Takes in an abbreviation of a city and reads its json files to a dataframe.
     """
+    
+    locs = {'input_master_data' : os.path.join('..', 'data', 'input', 'data_productie', 'master_data.json'),
+        'input_daily_BRU' : os.path.join('..', 'data', 'input', 'data_productie', 'daily_production', 'BRU'),
+        'input_daily_STO' : os.path.join('..', 'data', 'input', 'data_productie', 'daily_production', 'STO')}
+
     folder_daily = locs[f"input_daily_{city}"]
 
     # import all files which end with .json from folder
@@ -31,3 +40,25 @@ def remove_outlier(df):
     tresh_high = q3+1.5*iqr
     df_out = df.loc[(df > tresh_low) & (df < tresh_high)]
     return df_out, tresh_low, tresh_high
+
+def verdeling(perc_0, perc_out_low, perc_prod, perc_out_high, msr, avg, std, sample_size = 10 ** 3):
+    """
+    Functie genereert simulatie data obv parameters van betreffende stad voor n dagen
+    TODO: better option for 0 productie
+    """
+    distributions = [
+        {'type': np.random.normal, 'kwargs': {'loc': 0, 'scale': 1/100}},
+        {'type': np.random.uniform, 'kwargs': {'low': 0, 'high': tresh_low}},
+        {'type': np.random.normal, 'kwargs': {'loc': avg, 'scale': std}},
+        {'type': np.random.uniform, 'kwargs': {'low': tresh_high, 'high': msr}}
+    ]
+    coeff = np.array([perc_0, perc_out_low, perc_prod, perc_out_high])
+    coeff /= coeff.sum()
+    num_dis = len(distributions)
+
+    data = np.zeros((sample_size, num_dis))
+    for idx, distr in enumerate(distributions):
+        data[:, idx] = distr['type'](size = (sample_size,), **distr['kwargs'])
+    random_idx = np.random.choice(np.arange(num_dis), size = (sample_size,), p = coeff)
+    sim_data = data[np.arange(sample_size), random_idx]
+    return sim_data
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# StatVis
+# StatiVis
 Data Science Statistics &amp; Visualization Project
+
+[Link to github page](https://github.com/SarahDC7/StatVis)
+
+This repository is the result of an assignment to practice our **Statistical skills** for the course "Data Scientist" taught at Syntra AB.
+
+The assignment is explained in detail in the evaluatie_statistiek.pdf file.
+
+The repository consists of 2 folders, one for each part of this assignment.
+
+### stativis.yml
+
+Using this file you can create the environment I worked in to write this code.
+- [Helpful website](https://conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html#creating-an-environment-from-an-environment-yml-file)
